230e3593d3b18e4b45d794c23e39fcd1cf4d538f
gendalf1h <genadiy778953@gmail.com>
2026-04-04 20:13:47 +0400

    стили и ContactsController

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index() // contacts
+        {
+            return View();
+        }
+    }
+}
--- obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00cefec56f438eeb5efaabae8718886363599ca5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
