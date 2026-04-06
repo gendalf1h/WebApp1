ba0aef70099b08f8a3f82f7cccff3ef317568fb1
gendalf1h <genadiy778953@gmail.com>
2026-04-06 15:36:28 +0400

    добавил форму Обратная связь, стили для формы, класс Contact

---
+++ Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp1.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Message {  get; set; }
+
+
+
+    }
+}
